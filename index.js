318b34a5651c81edecbdf8c39cae6facf725f1b9
Enrico Marino <enrico.marino@email.com>
2013-01-06 10:49:07 +0100

    added index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+
+/* !
+ * parallel-map
+ * async parallel map
+ *
+ * @copyright 2013 Enrico Marino
+ * @license MIT
+ */
+
+/*
+ * Expose `parallel_map`
+ */
+
+module.exports = parallel_map;
+
+/**
+ * Map 'iterator' to each item in 'array' in parallel
+ * and call 'callback' when done
+ *
+ * @param {Array} array array
+ * @param {Function} iterator iterator
+ * @param {Function} callback callback
+ * @api public
+ */
+
+function parallel_map (array, iterator, callback) {
+  var results = [];
+  var completed = 0;
+  var len = array.length;
+  var i;
+
+  function complete (err, result) {
+    if (err) {
+      callback(err);
+      callback = function () {};
+      return;
+    }
+    results[index] = result;
+    completed += 1;
+    if (completed === len) {
+      callback(err, results);
+      return;
+    }
+  }
+
+  function iterate (value, index) {
+    iterator(value, complete);
+  }
+
+  for (i = 0; i < len; i += 1) {
+    iterate(array[i], i);
+  }
+}
